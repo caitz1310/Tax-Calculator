0f99a5013510ef30209682dae22d53ee0a805eb3
caitz1310 <142206866+caitz1310@users.noreply.github.com>
2023-12-04 22:09:40 +0200

    Add files via upload

---
+++ Tax Calculator.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace PRG521__FA3_Caitlin_Hermanus_20232198
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(444, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(622, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Amount : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sales Tax : ";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(332, 123);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Location = new System.Drawing.Point(332, 187);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(214, 22);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 401);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tax Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Tax Calculator.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG521__FA3_Caitlin_Hermanus_20232198
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        //User input textbox
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Error Handling
+            try
+            {
+
+            }
+            catch (FormatException)
+            {
+                //saves to ErrorMessage File file
+                File.Create(@"C:\Users\Caitlin\source\repos\PRG521__FA3_Caitlin_Hermanus_20232198\ErrorMessage.txt").Close();
+                {
+                    using (StreamWriter swError = File.AppendText(@"C:\Users\Caitlin\source\repos\PRG521__FA3_Caitlin_Hermanus_20232198\ErrorMessage.txt"))
+                    {
+                        swError.WriteLine(MessageBox.Show("Enter numbers only!"));
+                        swError.WriteLine("Enter numbers only!");
+                    }
+
+                }
+                
+            }
+           
+        }
+
+        //Submit button
+        //fix file saving
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Error Handling
+            try
+            {
+                //calculating the tax
+                double taxAmount = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(numericUpDown1.Value) / 100;
+                double total = Convert.ToDouble(textBox1.Text) + taxAmount;
+
+                //display the message
+                MessageBox.Show($"Tax on ${textBox1.Text} at {numericUpDown1.Value}% is ${taxAmount} \n The total is ${total}");
+
+                //saves to TaxCalucation file
+                File.Create(@"C:\Users\Caitlin\source\repos\PRG521__FA3_Caitlin_Hermanus_20232198\TaxCalculation.txt").Close();
+                {
+
+                    using (StreamWriter swTotal = File.AppendText(@"C:\Users\Caitlin\source\repos\PRG521__FA3_Caitlin_Hermanus_20232198\TaxCalculation.txt"))
+
+                        swTotal.WriteLine($"Tax on ${textBox1.Text} at {numericUpDown1.Value}% is ${taxAmount} \n The total is ${total}");
+                }
+            }
+            catch (FormatException)
+            {
+                //saves to ErrorMessage file
+                File.Create(@"C:\Users\Caitlin\source\repos\PRG521__FA3_Caitlin_Hermanus_20232198\ErrorMessage.txt").Close();
+                {
+                    using (StreamWriter swErrorTotal = File.AppendText(@"C:\Users\Caitlin\source\repos\PRG521__FA3_Caitlin_Hermanus_20232198\ErrorMessage.txt"))
+                    {
+
+                        swErrorTotal.WriteLine(MessageBox.Show("Error, file did not save"));
+                        swErrorTotal.WriteLine("Error, file did not save");
+                    }
+                }
+            }
+        }
+
+        //Clear button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //this clears the use input textbox
+            textBox1.Clear();
+        }
+
+        //Exit button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //this exits the whole system
+            this.Close();
+        }
+
+        //Tax Option
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //Error Handling
+            try
+            {
+                numericUpDown1.Maximum = 50000;
+                numericUpDown1.Minimum = 0;
+            }
+            catch (FormatException)
+            {
+                //saves to Errormessage file
+                File.Create(@"C:\Users\Caitlin\source\repos\PRG521__FA3_Caitlin_Hermanus_20232198\ErrorMessage.txt").Close();
+                using (StreamWriter swError = File.AppendText(@"C:\Users\Caitlin\source\repos\PRG521__FA3_Caitlin_Hermanus_20232198\ErrorMessage.txt"))
+                {         
+                    swError.WriteLine(MessageBox.Show("This connaot be converted to int"));
+                    swError.WriteLine("This connaot be converted to int");
+                }
+            }
+        }
+
+        //Select Amount Label
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Tax Amount Label
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
